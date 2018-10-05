946d25f2c502437b2d470038a06697712c7cbe34
Robin Wakeman <robin.wakeman92@gmail.com>
2018-10-04 21:10:42 -0400

    initial commit

---
+++ src/WordSearch.java
@@ -0,0 +1,20 @@
+
+public class WordSearch {
+
+	public static void main(String[] args) {
+		
+		System.out.print("What is the title? ");
+		
+		System.out.print("How many columns? ");
+		
+		System.out.print("How many rows? ");
+		
+		System.out.print("Would you like to choose a custom set of words? (y/n) ");
+		
+		// TODO - custom set - arraylist?
+		
+		
+
+	}
+
+}
