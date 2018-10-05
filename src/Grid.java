806507fb33b433b4015944d632254e448328739a
Robin Wakeman <robin.wakeman92@gmail.com>
2018-10-05 15:03:12 -0400

    duplicated word insertion functionality for vertical columns

--- src/Grid.java
+++ src/Grid.java
@@ -13,9 +13,12 @@ public class Grid {
 	}
 	
 	public void placeWords(String[] wordList) {
-		placeHorizontal(wordList);
+		String[] temp1 = {"cat", "dog", "cow"};
+		placeHorizontal(temp1);
+		placeVertical(temp1);
 	}
 
+	// TODO: handle case where a spot can't be found for the word, maybe after x tries
 	private boolean placeHorizontal(String[] wordList) {
 		
 		for (String word : wordList) {
@@ -36,26 +39,48 @@ public class Grid {
 				 }		
 			} while(!placeAvailable);
 		}
-		return false;
+		return true;
 	}
 	
-	private boolean placeVertical() {
-		return false;
+	private boolean placeVertical(String[] wordList) {
+		for (String word : wordList) {
+			//randomly generate placement spot
+			 // length of the word should be < spot to the end 
+			boolean placeAvailable;
+			do {
+				int x0 = random.nextInt(lastIndexAcross);
+				 int y0 = random.nextInt(lastIndexDown - word.length() + 2); // 1 or 2?
+				
+				 placeAvailable = spaceIsFree('v', x0, y0, word.length());
+				 //check if placement spot and row are available
+				 if (placeAvailable) {				 
+					 //place word
+					 for(int y=0; y<word.length(); y++) {
+						 grid[y0+y][x0] = word.charAt(y);
+					 } 
+				 }		
+			} while(!placeAvailable);
+		}
+		return true;
 	}
 	
 	private boolean placeDiagonal() {
 		return false;
 	}
 	
+	//TODO: handle case where identical characters can overlap
 	private boolean spaceIsFree(char direction, int x0, int y0, int wordLength) {
 		switch (direction) {
 			case 'h':
-				for(int x=x0; x<wordLength; x++) {
+				for(int x=x0; x<=wordLength; x++) {
 					if (grid[y0][x] != 0) return false;
 				}
 				return true;
 			case 'v': 
-				break;
+				for(int y=y0; y<=wordLength; y++) { // why does <= work instead of<????
+					if (grid[y][x0] != 0) return false; 
+				}
+				return true;
 			case 'd':
 				break;
 			default:
